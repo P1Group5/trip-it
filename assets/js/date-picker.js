e59c931efbf593f86e54277879152631970236ce
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-04-06 16:05:57 -0400

    Testing Modals
    Testing code for Modals. Currently not popping out but need it to do so.

--- assets/js/date-picker.js
+++ assets/js/date-picker.js
@@ -1,40 +1,49 @@
-$("#datePicker-going").datepicker({
+$("#datePicker-going").datepicker({});
 
+$("#datePicker-home").datepicker({});
+
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
+
+open.addEventListener("click", () => {
+  modal_container.classList.add("show");
 });
 
-$("#datePicker-home").datepicker({
-
+close.addEventListener("click", () => {
+  modal_container.classList.remove("show");
 });
 
-// MODALS
-var currentWeatherBtn = document.querySelector("#currentWeatherBtn");
-var weeklyWeatherBtn = document.querySelector("#weeklyWeatherBtn");
-var biWeeklyWeatherBtn = document.querySelector("#biWeeklyWeatherBtn");
-var monthlyWeatherBtn = document.querySelector("#monthlyWeatherBtn");
-var modalBg = document.querySelector(".modal-background");
+// // MODALS
+// var currentWeatherBtn = document.querySelector("#currentWeatherBtn");
+// var weeklyWeatherBtn = document.querySelector("#weeklyWeatherBtn");
+// var biWeeklyWeatherBtn = document.querySelector("#biWeeklyWeatherBtn");
+// var monthlyWeatherBtn = document.querySelector("#monthlyWeatherBtn");
+// var modalBg = document.querySelector(".modal-background");
 
-var modal = document.querySelector(".modal");
-var currentModal = document.querySelector(".current-weather-modal");
-var weeklyModal = document.querySelector(".weekly-weather-modal");
-var biweeklyModal = document.querySelector(".biweekly-weather-modal");
-var monthlyModal = document.querySelector(".monthly-weather-modal");
+// var modal = document.querySelector(".modal");
+// var currentModal = document.querySelector(".current-weather-modal");
+// var weeklyModal = document.querySelector(".weekly-weather-modal");
+// var biweeklyModal = document.querySelector(".biweekly-weather-modal");
+// var monthlyModal = document.querySelector(".monthly-weather-modal");
 
-currentWeatherBtn.addEventListener("click", function() {
-    currentModal.classList.add("is-active");
-});
+// document.currentWeatherBtn.addEventListener("click", function () {
+//   currentModal.style.display = "block";
+//   currentModal.classList.add("is-active");
+// });
 
-weeklyWeatherBtn.addEventListener("click", function() {
-    weeklyModal.classList.add("is-active");
-});
+// weeklyWeatherBtn.addEventListener("click", function () {
+//   weeklyModal.classList.add("is-active");
+// });
 
-biWeeklyWeatherBtn.addEventListener("click", function() {
-    biweeklyModal.classList.add("is-active");
-});
+// biWeeklyWeatherBtn.addEventListener("click", function () {
+//   biweeklyModal.classList.add("is-active");
+// });
 
-monthlyWeatherBtn.addEventListener("click", function() {
-    monthlyModal.classList.add("is-active");
-});
+// monthlyWeatherBtn.addEventListener("click", function () {
+//   monthlyModal.classList.add("is-active");
+// });
 
-modalBg.addEventListener("click", function() {
-    modal.classList.remove("is-active");
-});
+// modalBg.addEventListener("click", function () {
+//   modal.classList.remove("is-active");
+// });
