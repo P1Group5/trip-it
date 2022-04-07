63de13149b4207ccce0a1ce0892a532555332ace
Eric Smith <ericandrewsmith88@gmail.com>
2022-04-07 14:17:37 -0400

    improving display

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var arrivalAirport = "LAX";
 var flightContainerEL = document.querySelector("#flights-container");
 
 var getFlights = function(flights) {
-    var flightUrl = "https://www.expedia.com:443/api/flight/search?departureDate=" + departureDate + "&returnDate=" + returnDate + "&departureAirport=" + departureAirport + "&arrivalAirport=" + arrivalAirport + "&prettyPrint=true&maxOfferCount=9";
+    var flightUrl = "https://www.expedia.com:443/api/flight/search?departureDate=" + departureDate + "&returnDate=" + returnDate + "&departureAirport=" + departureAirport + "&arrivalAirport=" + arrivalAirport + "&maxOfferCount=9";
 
     fetch(flightUrl).then(function(response) {
         // request was successful
@@ -34,16 +34,16 @@ var displayFlights = function(flights) {
   //loop over offers
   for (var i = 0; i < flights.legs.length; i++) {
     // format offer info
-    var flightInfo = flights.legs[i].segments[0].airlineName + flights.legs[i].segments[0].departureTime;
+    var flightInfo = "<span class = 'flight-info'>" + flights.legs[i].segments[0].airlineName + "-" + flights.legs[i].segments[0].departureTime + "</span>";
 
     // create a container for each flight
     var flightEl = document.createElement("a");
-    flightEl.classList = "list-item flex-row justify-space-between align-center";
+    flightEl.classList = "flight-options column my-3 has-text-centered";
     flightEl.setAttribute("href", flights.legs[i].baggageFeesUrl)
 
     // create a span element to hold flight info
     var titleEl = document.createElement("span");
-    titleEl.textContent = flightInfo;
+    titleEl.innerHTML = flightInfo;
 
     // append container
     flightEl.appendChild(titleEl);
